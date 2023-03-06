ef8fb557d51b3cac3c2960422f503750909dcdeb
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-03-06 17:58:03 +0100

    first commit

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Post;
+use App\Models\Type;
+use Illuminate\Support\Str;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker){
+        $types = Type::all();
+
+        for ($i=0; $i<50; $i++){
+            $newPost = new Post();
+            $newPost->type_id = Type::inRandomOrder()->first()->id;
+            $newPost->title = $faker->unique()->realTextBetween(5, 20);
+            $newPost->slug = Str::slug($newPost->title);
+            $newPost->author = $faker->name();
+            $newPost->content = $faker->realTextBetween(200, 400);
+            $newPost->post_date = $faker->dateTimeThisYear();
+            $newPost->image_path = $faker->imageUrl(640, 480, 'animals', true);
+            $newPost->save();
+        }
+    }
+}
---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+    <h2 class="fs-4 text-secondary my-4 text-center">
+        {{ __('Dashboard') }}
+    </h2>
+    <div class="row justify-content-center">
+        <div class="col-10">
+            <div class="card text-center">
+                <div class="card-header mb-3">
+                        <p class="m-0">
+                            Status: {{ Auth::user()->userDetail->status }}
+                        </p>
+                    </div>
+                    <div class="card-title">
+                        <h4>
+                            {{ Auth::user()->name }}
+                        </h4>
+                    </div>
+                    <div class="card-image">
+                        <img src="{{ Auth::user()->userDetail->profile_image }}" alt="" class="img-thumbnail w-25 d-inline-block">
+                    </div>
+                    <div class="card-body">
+                        <h6>
+                            email address : {{ Auth::user()->email }}
+                        </h6>
+                    </div>
+                    <div class="card-footer">
+                        <h6>
+                            Bio:
+                        </h6>
+                        <p class=" fst-italic">
+                            {{ Auth::user()->userDetail->bio }}
+                        </p>
+
+                    </div>
+                </div>
+        </div>
+    </div>
+</div>
+@endsection
